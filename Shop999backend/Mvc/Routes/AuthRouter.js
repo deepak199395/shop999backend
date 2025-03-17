6e67982bff3331baf421b7448ab8044b7db1c22d
Deepak yadav <deepak.yadav11@nipponindiaim.com>
2025-03-17 08:23:04 +0530

    products get and creayte api

--- Shop999backend/Mvc/Routes/AuthRouter.js
+++ Shop999backend/Mvc/Routes/AuthRouter.js
@@ -1,22 +1,16 @@
 const express =require("express");
 const { createUserController, getUserController, LoginController } = require("../Controllers/AuthController");
-const { createProject } = require("../Controllers/ProductController");
-
+const { createProject, getProductsDetails } = require("../Controllers/ProductController");
 const router=express();
-
-
 // router GET || POST || PUT || DELETE 
-
 //post
 router.post("/create",createUserController)
-
 //get
 router.get("/getUser",getUserController)
-
 // LOGIN
 router.post("/login",LoginController)
-
 // prodct API routes
-router.put("/createprocts",createProject)
-
+router.post("/CreateProducts",createProject)
+// get products details api 
+router.get("/getProducts",getProductsDetails)
 module.exports=router;
