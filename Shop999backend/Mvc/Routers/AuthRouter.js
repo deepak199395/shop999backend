417a262b5a27da8d5bce9a78f566595bd174383f
Deepak yadav <deepak19939426@gmail.com>
2025-04-23 10:21:55 +0530

    Change Api structure and url3

--- Shop999backend/Mvc/Routers/AuthRouter.js
+++ Shop999backend/Mvc/Routers/AuthRouter.js
@@ -30,43 +30,43 @@ const PROJECT_API_PREFIX = "/api/v1/projects";
 const MOVIE_API_PREFIX = "/api/v1/movies";
 const LIVESHOW_API_PREFIX = "/api/v1/liveshows";
 
-// ====================== Auth Routes ======================
-router.post(`${AUTH_API_PREFIX}/create`, createUserController);
-router.get(`${AUTH_API_PREFIX}/list`, getUserController);
-router.post(`${AUTH_API_PREFIX}/login`, LoginController);
-router.put(`${AUTH_API_PREFIX}/update/:id`, updateUserController);
-router.delete(`${AUTH_API_PREFIX}/delete/:id`, deleteController);
+// ====================== Auth Routes ========================================
+router.post(`${AUTH_API_PREFIX}/Create-User/api1`, createUserController);
+router.get(`${AUTH_API_PREFIX}/GetUser-list/api2`, getUserController);
+router.post(`${AUTH_API_PREFIX}/Login-User/api3`, LoginController);
+router.put(`${AUTH_API_PREFIX}/Update-User/api4/:id`, updateUserController);
+router.delete(`${AUTH_API_PREFIX}/Delete-User/api5/:id`, deleteController);
 
-// ====================== Product Routes ======================
-router.post(`${PRODUCT_API_PREFIX}/create`, createProject);
-router.get(`${PRODUCT_API_PREFIX}/list`, getProductsDetails);
+// ====================== Product Routes ======================================
+router.post(`${PRODUCT_API_PREFIX}/Create-Products/api6`, createProject);
+router.get(`${PRODUCT_API_PREFIX}/GetProducts-list/api7`, getProductsDetails);
 
 // ====================== IPL Ticket Routes ======================
-router.post(`${TICKET_API_PREFIX}/create`, CreateTickets);
-router.get(`${TICKET_API_PREFIX}/list`, getTicketInfo);
+router.post(`${TICKET_API_PREFIX}/Create-IPLTickets/api8`, CreateTickets);
+router.get(`${TICKET_API_PREFIX}/GetIPLTickets-list/api9`, getTicketInfo);
 
 // ====================== Booking Routes ======================
-router.post(`${BOOKING_API_PREFIX}/create`, createBooking);
-router.get(`${BOOKING_API_PREFIX}/list`, getBookingControllers);
+router.post(`${BOOKING_API_PREFIX}/Create-Booking/api10`, createBooking);
+router.get(`${BOOKING_API_PREFIX}/Get-Booking-list/api11`, getBookingControllers);
 
 // ====================== Experience Routes ======================
-router.post(`${EXPERIENCE_API_PREFIX}/create`, createExperience);
-router.get(`${EXPERIENCE_API_PREFIX}/list`, getExperienceController);
+router.post(`${EXPERIENCE_API_PREFIX}/Create-Experience/api12`, createExperience);
+router.get(`${EXPERIENCE_API_PREFIX}/GetExperience-list/api13`, getExperienceController);
 
 // ====================== Stack Routes ======================
-router.post(`${STACK_API_PREFIX}/create`, createStackController);
-router.get(`${STACK_API_PREFIX}/list`, getStackController);
+router.post(`${STACK_API_PREFIX}/Create-TeachStack/api14`, createStackController);
+router.get(`${STACK_API_PREFIX}/GetTeachStack-list/api15`, getStackController);
 
 // ====================== Project Routes ======================
-router.post(`${PROJECT_API_PREFIX}/create`, createProjectController);
-router.get(`${PROJECT_API_PREFIX}/list`, getProjectController);
+router.post(`${PROJECT_API_PREFIX}/Create-Projects/api16`, createProjectController);
+router.get(`${PROJECT_API_PREFIX}/GetProjects-list/api17`, getProjectController);
 
 // ====================== Movie Routes ======================
-router.post(`${MOVIE_API_PREFIX}/create`, CreateMovieController);
-router.get(`${MOVIE_API_PREFIX}/list`, getMovieDetailsController);
+router.post(`${MOVIE_API_PREFIX}/Create-MovieInfo/api18`, CreateMovieController);
+router.get(`${MOVIE_API_PREFIX}/GetMovieInfo-list/api19`, getMovieDetailsController);
 
 // ====================== Live Show Routes ======================
-router.post(`${LIVESHOW_API_PREFIX}/create`, CreateLiveController);
-router.get(`${LIVESHOW_API_PREFIX}/list`, getLiveshowController);
+router.post(`${LIVESHOW_API_PREFIX}/Create-LiveShow/api20`, CreateLiveController);
+router.get(`${LIVESHOW_API_PREFIX}/GetLiveShow-list/api21`, getLiveshowController);
 
 module.exports = router;
