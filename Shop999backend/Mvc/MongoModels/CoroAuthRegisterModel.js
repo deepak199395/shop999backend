815aae10866a8f91cc612a856b15ccf251879228
deepak199395 <deepak19939426@gmail.com>
2025-11-08 16:24:10 +0530

    fox_emi_api42 update

--- Shop999backend/Mvc/MongoModels/CoroAuthRegisterModel.js
+++ Shop999backend/Mvc/MongoModels/CoroAuthRegisterModel.js
@@ -4,20 +4,16 @@ const mongoose = require('mongoose');
 const coroAuthRegisterSchema = new mongoose.Schema({
   fullName: {
     type: String,
-    required: [true, 'Full name is required'],
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
     unique: true,
   },
   phone: {
     type: String,
-    required: [true, 'Phone number is required'],
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
   },
   country: {
     type: String,
